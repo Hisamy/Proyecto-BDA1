c9725272d84329aefcb88be8fde1a8963ba7f960
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-02-17 15:16:07 -0700

    RetiroSinCuentaDialog

--- BancoMB/src/main/java/org/itson/bda/BancoMB/bancoMB/BancoMB.java
+++ BancoMB/src/main/java/org/itson/bda/BancoMB/bancoMB/BancoMB.java
@@ -19,7 +19,7 @@ public class BancoMB {
         IConexion conexion = new Conexion(cadenaConexion, usuario, password);
         IClientesDAO clientesDAO = new ClientesDAO(conexion);
         IRetiroSinCuentaDAO retirosSinCuentaDAO = new RetirosSinCuentaDAO(conexion);
-        RetiroSinCuentaForm clientesForm = new RetiroSinCuentaForm(clientesDAO);
-        clientesForm.setVisible(true);
+        RetiroSinCuentaDialog retiroSinCuentaDialog = new RetiroSinCuentaDialog(clientesDAO);
+        retiroSinCuentaDialog.setVisible(true);
     }
 }
