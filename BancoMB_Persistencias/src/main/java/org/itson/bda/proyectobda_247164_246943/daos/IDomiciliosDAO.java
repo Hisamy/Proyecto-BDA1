babed154898e6a5c9f62ca91f51e4d2b70cc3aa6
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-02-17 19:41:15 -0700

    DomicilioNuevoDTO

---
+++ BancoMB_Persistencias/src/main/java/org/itson/bda/proyectobda_247164_246943/daos/IDomiciliosDAO.java
@@ -0,0 +1,16 @@
+
+package org.itson.bda.proyectobda_247164_246943.daos;
+
+import java.util.List;
+import org.itson.bda.proyectobda_247164_246943.bancoMB.Domicilio;
+import org.itson.bda.proyectobda_247164_246943.dtos.DomicilioNuevoDTO;
+import org.itson.bda.proyectobda_247164_246943.excepciones.PersistenciaException;
+
+
+
+
+public interface IDomiciliosDAO {
+    List<Domicilio> consultar() throws PersistenciaException;
+
+    Domicilio agregar(DomicilioNuevoDTO socioNuevo) throws PersistenciaException;
+}
---
+++ BancoMB_Persistencias/src/main/java/org/itson/bda/proyectobda_247164_246943/dtos/DomicilioNuevoDTO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.bda.proyectobda_247164_246943.dtos;
+
+/**
+ *
+ * @author hisam
+ */
+public class DomicilioNuevoDTO {
+    
+}
