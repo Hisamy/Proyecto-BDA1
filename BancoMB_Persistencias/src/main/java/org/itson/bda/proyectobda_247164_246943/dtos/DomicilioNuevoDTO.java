7682403c5392f4af21848cee03d65b9c75723412
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-02-17 19:43:55 -0700

    Update DomicilioNuevoDTO

--- BancoMB_Persistencias/src/main/java/org/itson/bda/proyectobda_247164_246943/dtos/DomicilioNuevoDTO.java
+++ BancoMB_Persistencias/src/main/java/org/itson/bda/proyectobda_247164_246943/dtos/DomicilioNuevoDTO.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.itson.bda.proyectobda_247164_246943.dtos;
 
-/**
- *
- * @author hisam
- */
+
 public class DomicilioNuevoDTO {
+    private int numeroDomicilio;
+    private String calle, colonia;
+    private int CP;
+
+    public int getNumeroDomicilio() {
+        return numeroDomicilio;
+    }
+
+    public void setNumeroDomicilio(int numeroDomicilio) {
+        this.numeroDomicilio = numeroDomicilio;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public int getCP() {
+        return CP;
+    }
+
+    public void setCP(int CP) {
+        this.CP = CP;
+    }
+    
     
 }
