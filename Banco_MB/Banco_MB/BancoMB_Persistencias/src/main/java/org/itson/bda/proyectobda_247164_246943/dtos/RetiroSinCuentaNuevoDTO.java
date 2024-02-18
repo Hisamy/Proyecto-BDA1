dfc0b2225640f0b47e90ec9bca33cf4277e862d1
Hisamy <hisamy.cinco247164@potros.itson.edu.mx>
2024-02-17 19:21:52 -0700

    Persistencias

---
+++ Banco_MB/Banco_MB/BancoMB_Persistencias/src/main/java/org/itson/bda/proyectobda_247164_246943/dtos/RetiroSinCuentaNuevoDTO.java
@@ -0,0 +1,44 @@
+
+package org.itson.bda.proyectobda_247164_246943.dtos;
+
+import java.sql.Date;  
+import java.time.LocalDateTime;
+
+public class RetiroSinCuentaNuevoDTO {
+    private int folio;
+    private int contrasenia;
+    private Float monto;
+    private LocalDateTime fechaGenerada;  
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public int getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(int contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public LocalDateTime getFechaGenerada() {
+        return fechaGenerada;
+    }
+
+    public void setFechaGenerada(LocalDateTime fechaGenerada) {
+        this.fechaGenerada = fechaGenerada;
+    }
+
+    public Float getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Float monto) {
+        this.monto = monto;
+    }
+}
